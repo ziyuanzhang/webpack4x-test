5fdf9a443b655e71e4979f8289c40a34c8b23a9b
ziyuzhan <ziyuzhan@cisco.com>
2018-09-14 13:21:57 +0800

    test

--- webpack.base.js
+++ webpack.base.js
@@ -23,16 +23,6 @@ module.exports = {
               }
             }]
         },
-        /* {
-          test: /\.svg$/,
-          use: [{
-              loader: 'file-loader',
-              options:{
-                name:"[name].[ext]",
-                outputPath: 'images/'
-              }
-            }]
-        }, */
         {
           test:/\.(woff|woff2|eot|ttf|otf)$/,
           use:[{
